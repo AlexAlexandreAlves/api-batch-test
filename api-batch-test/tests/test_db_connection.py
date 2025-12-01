8dc1904195a803f44410d1440b30aa790a07d1ff
Alex <alexalexandrealves@gmail.com>
2025-12-01 17:14:26 -0300

    feat: initial commit

---
+++ api-batch-test/tests/test_db_connection.py
@@ -0,0 +1,13 @@
+import pytest
+
+@pytest.mark.testdb
+def test_users(db_connection):
+    people = db_connection.execute_query_dict(
+        "SELECT * FROM people WHERE id = %s",
+        (1,)
+    )
+    assert len(people) > 0
+    
+    person = people[0]
+    assert person["fname"] == "Alice"
+    assert person["age"] == 30
